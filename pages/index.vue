dfc9b9092debcffc94a54a89e4643c84a725398b
Shiven Shukla <shivenshukla03@gmail.com>
2023-06-29 21:36:35 -0700

    add components to home page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+	<section>
+		<h1 class="text-5xl font-bold mt-20">Shiven Shukla</h1>
+		<p class="text-base text-gray 900 p-2 italic">Junior Software Engineer at Planview, Inc.</p>
+	</section>
+	<section class="flex flex-col md:flex-row">
+		<div class="md:w-3/4">
+			<h2 class="text-3xl font-bold mt-8">About Me</h2>
+			<p class="text-lg py-2">
+				Hi! I am a junior software engineer with a background in Computer Science.
+			</p>
+		</div>
+		<img src="~/assets/images/goku.jpg" class="w-1/2 md:max-w-sm p-8 mx-auto"/>
+	</section>
+</template>
