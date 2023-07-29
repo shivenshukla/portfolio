54b2656f9cf8b928134c212f2af99c40ed147485
Shiven Shukla <shivenshukla03@gmail.com>
2023-07-28 20:57:38 -0700

    change default text color for dark mode to white

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <Head>
     <Title>Shiven Shukla</Title>
   </Head>
-  <div class="flex flex-col min-h-screen dark:bg-zinc-800 dark:text-gray-200">
+  <div class="flex flex-col min-h-screen dark:bg-zinc-800 dark:text-white">
     <SiteHeader />
     <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
       <NuxtPage />
