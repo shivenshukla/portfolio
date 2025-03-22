78e63274bf09d30f8d7ccc95b0f5954a04edf887
Shiven Shukla <shivenshukla03@gmail.com>
2025-03-21 23:27:30 -0700

    update background color of body

--- app.vue
+++ app.vue
@@ -2,13 +2,15 @@
   <Head>
     <Title>Shiven Shukla</Title>
   </Head>
-  <div class="flex flex-col min-h-screen dark:bg-zinc-800 dark:text-white">
-    <SiteHeader />
-    <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 grow items-start w-full">
-      <NuxtPage />
-    </main>
-    <SiteFooter />
-  </div>
+  <body class="dark:bg-zinc-800">
+    <div class="flex flex-col min-h-screen dark:bg-zinc-800 dark:text-white">
+      <SiteHeader />
+      <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 grow items-start w-full">
+        <NuxtPage />
+      </main>
+      <SiteFooter />
+    </div>
+  </body>
 </template>
 
 <style>
