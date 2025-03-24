3ae72cef9ffe7de15ffcdc6b6c0c2d723ffe23f1
Shiven Shukla <shivenshukla03@gmail.com>
2025-03-24 14:43:02 -0700

    fix background color of error page

--- error.vue
+++ error.vue
@@ -2,16 +2,18 @@
 	<Head>
 		<Title>Shiven Shukla</Title>
 	</Head>
-	<main class="grid min-h-screen place-items-center bg-white dark:bg-zinc-800 px-6 py-24 sm:py-32 lg:px-8">
-		<div class="text-center">
-			<p class="text-9xl font-bold text-sky-500">404</p>
-			<h1 class="mt-4 text-4xl font-semibold tracking-tight text-gray-900 dark:text-white">Page Not Found</h1>
-			<div class="mt-10 flex items-center justify-center gap-x-6">
-				<a href="/"
-					class="rounded-md px-3.5 text-sky-500 dark:text-gray-200 py-2.5 text-md font-bold shadow-sm bg-sky-100 dark:bg-zinc-700 hover:ring-4 ring-sky-500">Go
-					Back Home
-				</a>
+	<body class="dark:bg-zinc-800">
+		<main class="grid min-h-screen place-items-center bg-white dark:bg-zinc-800 px-6 py-24 sm:py-32 lg:px-8">
+			<div class="text-center">
+				<p class="text-9xl font-bold text-sky-500">404</p>
+				<h1 class="mt-4 text-4xl font-semibold tracking-tight text-gray-900 dark:text-white">Page Not Found</h1>
+				<div class="mt-10 flex items-center justify-center gap-x-6">
+					<a href="/"
+						class="rounded-md px-3.5 text-sky-500 dark:text-gray-200 py-2.5 text-md font-bold shadow-sm bg-sky-100 dark:bg-zinc-700 hover:ring-4 ring-sky-500">Go
+						Back Home
+					</a>
+				</div>
 			</div>
-		</div>
-	</main>
+		</main>
+	</body>
 </template>
