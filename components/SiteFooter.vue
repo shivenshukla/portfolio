8033a5cfb6f83c47eb281e4b6de3cde682ccb0e2
Shiven Shukla <shivenshukla03@gmail.com>
2026-04-21 19:23:03 -0700

    udpate experience

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer>
 		<section class="mx-auto max-w-7xl pt-6 pb-4 flex flex-row justify-center text-gray-700 dark:text-gray-200">
-			<p>&copy; Shiven Shukla 2025</p>
+			<p>&copy; Shiven Shukla 2026</p>
 		</section>
 	</footer>
 </template>
